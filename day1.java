f46b56d9267af0777e99b95e837ec41736bc96b5
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-08-12 12:52:08 +0530

    minor

--- day1.java
+++ day1.java
@@ -1,8 +1,27 @@
-public class day1{
-    public static void main (String args[]){
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
-    }
-}
+// public class day1{
+//     public static void main (String args[]){
+//         System.out.println("****");
+//         System.out.println("***");
+//         System.out.println("**");
+//         System.out.println("*");
+//     }
+// } 
+
+// public class day1{
+//     public static void main (String args[]){
+//         byte A= 5;
+//         Byte b = 6;
+//         System.out.println(A+b);
+//     }
+// }
+// import java.util.*;
+// public class day1{
+//     public static void main (String args[]){
+//      Scanner sc= new Scanner(System.in);
+//     //  String a = sc.next();
+//     // String a = sc.nextLine();
+//     // int a =sc.nextInt();
+//     float a = sc.nextFloat();
+//      System.out.println(a);
+//     }
+// } 
