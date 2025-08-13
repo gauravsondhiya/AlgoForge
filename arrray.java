287dd6526b56dcbba297dd927de500bbb7777389
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-08-13 20:41:00 +0530

    minor

---
+++ arrray.java
@@ -0,0 +1,10 @@
+public class arrray {
+    public static void main (String args[]){
+        int no[] = new int[3];
+          int number[]= {1,2,3,4};
+          String name[]={"gaurav","sondhiya","hello"};
+          float values[]={5.5f,6.6f,7.8f};
+
+        System.out.print(values[2]);
+    }
+}
--- day2.java
+++ day2.java
@@ -33,3 +33,57 @@
 //     }
 // }
 
+//   find factorial 
+// import java.util.*;
+// public class day2{
+//     public static int factor(int a){
+//         int out=1;
+//         for(int i=1;i<=a; i++){
+//            out =out*i;
+//         }
+//         return out;
+//     }
+//     public static void main (String args[]){
+//      Scanner sc = new Scanner(System.in);
+//      int a = sc.nextInt();
+//      System.out.print(factor(a));
+//     }
+// }
+
+// find binomial coffiencnt
+
+// public class day2{
+
+   
+//     public static int factorial(int n){
+//         int a = 1;
+//         for(int i=1; i<=n; i++){
+//            a= a*i; 
+//         }
+//         return a;
+//     }
+    
+//     public static void main (String args[]){
+//         int n=5;
+//         int r= 2;
+//         int nvalue = factorial(n);
+//         int rvalue =factorial(r);
+//         int down = n-r;
+//         int downfactorial = factorial(down);
+//         int ans = nvalue /(rvalue * downfactorial); 
+//         System.out.print(ans);
+//     }
+// }
+
+// public class day2{
+//     public static String prime(int a){
+//         if(a%2==0 || a%3==0){
+//             return "not prime";
+//         }else{
+//             return "Prime";
+//         }
+//     }
+//     public static void main (String args[]){
+//         System.out.print(prime(2));
+//     }
+// }
