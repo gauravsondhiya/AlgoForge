dcf51353ab6a34a4a25ff58c4f9ef3c337571049
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-08-15 07:17:07 +0530

    array done

--- arrray.java
+++ arrray.java
@@ -1,10 +1,44 @@
-public class arrray {
-    public static void main (String args[]){
-        int no[] = new int[3];
-          int number[]= {1,2,3,4};
-          String name[]={"gaurav","sondhiya","hello"};
-          float values[]={5.5f,6.6f,7.8f};
+// public class arrray {
+//     public static void main (String args[]){
+//         int no[] = new int[3];
+//           int number[]= {1,2,3,4};
+//           String name[]={"gaurav","sondhiya","hello"};
+//           float values[]={5.5f,6.6f,7.8f};
 
-        System.out.print(values[2]);
-    }
-}
+//         System.out.print(values[2]);
+//     }
+// }
+
+// public class arrray {
+//   public static int add(int num[]) {
+//     for (int i = 0; i < num.length; i++) {
+//       System.out.print(i);
+//     }
+//     return -1;
+//   }
+//   public static void main(String args[]) {
+//     int num[] = { 1, 2, 3, 4 };
+//      add(num);
+//   }
+// }
+
+// add number
+
+// public class arrray{
+
+//    public static void add(int num[]){
+//      int sum =0;
+//      for(int i=0; i<num.length; i++){
+//         num[i]=num[i]+ 1;
+//      }
+//     // System.out.print(sum);
+//    }
+//   public static void main (String args[]){
+//      int num[]={1,2,3,4};
+//      add(num);
+//      for(int i=0; i<num.length; i++){
+//       System.out.print(num[i]);
+//      }
+//   }
+// }
+
